b86978e450e133f2154c528b7e6b14762a185b41
Valeska <valeskaedu1@gmail.com>
2024-09-09 17:31:30 +0530

    completed home assigment

--- valeska_24/src/PersonalDetails.js
+++ valeska_24/src/PersonalDetails.js
@@ -7,64 +7,58 @@ class PersonalDetails extends React.Component{
             <div className="box" > 
                 <h1 className="Details " >Details</h1>
                 <div className="PD ">
-                     <h2 className="head">Personal Detail </h2>
-                    <p>Name : Valeska Rosario</p>
-                    <p>PID : 221100</p>
-                    <p>Class: TE IT B</p>
-                    <p>College : St Francis Institue of Technology </p>
-                    </div>
+                    <h2 className="head">Personal Detail </h2>
+                        <p>Name : Valeska Rosario</p>
+                        <p>PID : 221100</p>
+                        <p>Class: TE IT B</p>
+                        <p>College : St Francis Institue of Technology </p>
+                </div>
                     
-                    <div className="AD ">
+                <div className="AD ">
                     <h2  className="head">Academic Detail </h2>
+                    
                     <table border={1}>
                     <thead>
                         <tr>
-                    <th>Sr.no </th>
-                    <th>Exam Passed</th>
-                    <th>College / Institue </th>
-                    <th>Percentage/ Pointers </th>
-                    </tr>
-                    </thead>
+                            <th>Sr.no </th>
+                            <th>Exam Passed</th>
+                            <th>College / Institue </th>
+                            <th>Percentage/ Pointers </th>
+                        </tr>
 
+                    </thead>
                     <tbody>
                         <tr>
-                        <td> 1 </td>
-                        <td> SSC </td>
-                        <td> St Charles High School </td>
-                        <td> 89.4%</td>
+                            <td> 1 </td>
+                            <td> SSC </td>
+                            <td> St Charles High School </td>
+                            <td> 89.4%</td>
                         </tr>
 
                         <tr>
-                        <td> 2</td>
-                        <td> HSC </td>
-                        <td> St Andrews College of Arts Science and Commerce </td>
-                        <td> 89.4 %</td>
+                            <td> 2</td>
+                            <td> HSC </td>
+                            <td> St Andrews College of Arts Science and Commerce </td>
+                            <td> 89.4 %</td>
                         </tr>
 
                         <tr>
-                        <td> 3</td>
-                        <td> French Certificate </td>
-                        <td> University of Mumbai </td>
-                        <td> 71 %</td>
+                            <td> 3</td>
+                            <td> French Certificate </td>
+                            <td> University of Mumbai </td>
+                            <td> 71 %</td>
                         </tr>
 
                         <tr>
-                        <td> 4</td>
-                        <td> Sem 4 IT   </td>
-                        <td> St Francis Institue of Technology  </td>
-                        <td> 9.6 CGPA</td>
+                            <td> 4</td>
+                            <td> Sem 4 IT   </td>
+                            <td> St Francis Institue of Technology  </td>
+                            <td> 9.6 CGPA</td>
                         </tr>
-
-                        
+    
                     </tbody>
-                </table>
+                    </table>
                 </div>
-
-
-
-
-
-
             </div>
             
         );
